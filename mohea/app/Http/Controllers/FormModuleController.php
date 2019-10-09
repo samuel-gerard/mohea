b966d004fdbd0edb477711b143bf7303259c3494
Coralie <coraliebach67@gmail.com>
2019-10-09 21:00:14 +0200

    Init resource for form, menu and table

---
+++ mohea/app/Http/Controllers/FormModuleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FormModule;
+use Illuminate\Http\Request;
+
+class FormModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FormModule  $formModule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FormModule $formModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\FormModule  $formModule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(FormModule $formModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\FormModule  $formModule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, FormModule $formModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\FormModule  $formModule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(FormModule $formModule)
+    {
+        //
+    }
+}
---
+++ mohea/app/Http/Controllers/TableModuleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TableModule;
+use Illuminate\Http\Request;
+
+class TableModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TableModule  $tableModule
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TableModule $tableModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TableModule  $tableModule
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TableModule $tableModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TableModule  $tableModule
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TableModule $tableModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TableModule  $tableModule
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TableModule $tableModule)
+    {
+        //
+    }
+}

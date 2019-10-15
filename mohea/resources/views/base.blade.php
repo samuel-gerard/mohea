1cb6cb0ce1bdf5e50a3e148666422c6997f261dc
Coralie <coraliebach67@gmail.com>
2019-10-15 11:23:58 +0200

    Ajoute l'avatar

--- mohea/resources/views/base.blade.php
+++ mohea/resources/views/base.blade.php
@@ -24,7 +24,9 @@
                         <a href="{{ route('dashboard') }}">Dashboard</a>
                         <div class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                <img src="{{ Auth::user()->avatar }}" alt="" width="20px">
+                                {{ Auth::user()->name }}
+                                 <span class="caret"></span>
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">

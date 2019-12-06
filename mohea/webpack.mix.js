6a3d5d905a501a4b6b70271dbd084fe36c4ac82d
Coralie <coraliebach67@gmail.com>
2019-12-06 11:58:18 +0100

    Modifie le webpack mix

--- mohea/webpack.mix.js
+++ mohea/webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
-   .react('resources/js/modules/table/TableApp', 'public/js/modules')
+mix.js('resources/js/app.js', 'public/js')
+   .js('resources/js/modules/table/TableApp', 'public/js/modules')
    .sass('resources/sass/app.scss', 'public/css');

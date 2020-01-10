1404dc597801f8aa432e36b8914f746b0b5a1d6a
Olivier <olivier.roessel@etu.unistra.fr>
2020-01-10 16:04:14 +0100

    Added style and home page

--- mohea/public/js/app.js
+++ mohea/public/js/app.js
@@ -104,6 +104,41 @@
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+if (document.getElementById("header") && document.getElementById("home-screen-logo")) {
+  document.addEventListener("scroll", function () {
+    if (document.documentElement.scrollTop != 0 || document.body.scrollTop != 0) {
+      document.getElementById("header").classList.add("min");
+    } else {
+      document.getElementById("header").classList.remove("min");
+    }
+
+    if (document.getElementById("home-screen-logo").offsetTop - document.documentElement.scrollTop - document.body.scrollTop <= -50) {
+      document.getElementById("header").classList.add("show-logo");
+    } else {
+      document.getElementById("header").classList.remove("show-logo");
+    }
+  }, {
+    passive: true
+  });
+}
+
+if (document.getElementById("home-cta")) {
+  document.getElementById("home-cta").addEventListener("click", function () {
+    scrollTo('home-presentation');
+  });
+}
+
+function scrollTo(el) {
+  if (typeof el == "string") {
+    document.getElementById(el).scrollIntoView({
+      behavior: 'smooth'
+    });
+  } else {
+    el.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+}
 
 /***/ }),
 
@@ -136,9 +171,9 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/LpWeb/Mohea/mohea.bach.mmi-unistra.fr/mohea/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /var/www/html/LpWeb/Mohea/mohea.bach.mmi-unistra.fr/mohea/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /var/www/html/LpWeb/Mohea/mohea.bach.mmi-unistra.fr/mohea/resources/sass/form.scss */"./resources/sass/form.scss");
+__webpack_require__(/*! C:\wamp64\www\mohea.bach.mmi-unistra.fr\mohea\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\wamp64\www\mohea.bach.mmi-unistra.fr\mohea\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\wamp64\www\mohea.bach.mmi-unistra.fr\mohea\resources\sass\form.scss */"./resources/sass/form.scss");
 
 
 /***/ })

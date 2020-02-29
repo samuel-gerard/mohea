8e471558b57824642a8e7756590cb276218bdcbc
Olivier <olivier.roessel@etu.unistra.fr>
2020-02-29 12:34:58 +0100

    New favicon

--- mohea/resources/views/base.blade.php
+++ mohea/resources/views/base.blade.php
@@ -3,17 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        {{--  Verification Google  --}}
         <meta name="google-site-verification" content="lSEoBabnAq1DqFbV7-kNKBx-t1bhtzQn2zQCHjyPURI" />
-        
         <title>
             @yield('title')
         </title>
-
-        <!-- Fonts -->
+        <link rel="shortcut icon" type="image/png" href="favicon.png">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         @yield('style')
     </head>
